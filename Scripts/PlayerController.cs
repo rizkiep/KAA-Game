7588109d021216623cd3b8efe3f861e848e24d20
rizkiep <rizkiep12@gmail.com>
2022-07-13 17:06:53 +0700

    Menambahkan script dan build game dalam bentuk rar

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     public int score=0;
-    public AudioSource benar;
-    public AudioSource salah;
     public Text scoreText;
     // Start is called before the first frame update
     void Start()
@@ -23,16 +22,11 @@ public class PlayerController : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D collision){
         if(collision.tag=="KAA"){
-            benar=GetComponent<AudioSource>();
             score++;
-            benar.Play();
             scoreText.text=score.ToString();
         }
         else if(collision.tag=="NonKAA"){
-            salah=GetComponent<AudioSource>();
-            score--;
-            salah.Play();
-            scoreText.text=score.ToString();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1);
         }
     }
 }
